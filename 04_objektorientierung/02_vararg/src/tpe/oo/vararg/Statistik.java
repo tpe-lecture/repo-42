48b7e723928e9235476c817296338798c44f8f2e
Anouar <Anouar@134.155.234.107>
2016-12-04 15:52:52 +0100

    fertig

--- 04_objektorientierung/02_vararg/src/tpe/oo/vararg/Statistik.java
+++ 04_objektorientierung/02_vararg/src/tpe/oo/vararg/Statistik.java
@@ -1,5 +1,7 @@
 package tpe.oo.vararg;
 
+import java.util.Arrays;
+
 /**
  * Einfache Statistik-Funktionen.
  */
@@ -10,6 +12,45 @@ public final class Statistik {
      */
     private Statistik() {
     }
-    
+    /**
+     * median berechnen.
+     * @param array die liste wo raus wir den median berechnen werden.
+     * @return gibt den wert des medians zurück.
+     */
+    public static int median(int...array) {
+
+        Arrays.sort(array);
+        if (array.length % 2 != 0) {
+            int a = array.length;
+            int c = a / 2;
+            int median = array[c];
+            return median;
+        }
+        else {
+            int c = ((array.length) / 2) - 1;
+            int median = ((array[c] + array[c + 1]) / 2);
+            return median;
+        }
+    }
+    /**
+     * median beredchnen.
+     * @param array double array.
+     * @return gibt der wert des medians zurück.
+     */
+    public static double median(double...array) {
+        Arrays.sort(array);
+        if (array.length % 2 != 0) {
+            double a = array.length;
+            double c = a / 2;
+            double median = array[(int) c];
+            return median;
+        }
+        else {
+            double c = ((array.length) / 2) - 1;
+            double median = ((array[(int) c] + array[(int) (c + 1)]) / 2);
+            return median;
+        }
+    }
+
     // Bitte fügen Sie Ihre Methoden hier ein
 }
